04a6218af7a8255564837db49946d8f28ce3e954
vmatveev <vmatveevspam@gmail.com>
2021-08-27 05:08:29 -0400

    Timeline section done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -6,6 +6,7 @@ import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './Techn
 const Technologies = () =>  (
   <Section id="tech">
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range of Technologies in the web dev field world. from backend to design.
